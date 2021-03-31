2204cfccd0b41360ee33a5295bd173094ade6a8c
sharphurt <pavellukoanov679@gmail.com>
2021-03-31 20:52:08 +0500

    Added link to sign up on login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from user_profile import views
+
 urlpatterns = [
+    path('', views.user_signin),
     path('admin/', admin.site.urls),
     path('', include('user_profile.urls')),
     path('', include('notebook.urls'))
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('/', views.user_signin),
     path('signin/', views.user_signin, name="signin"),
     path('logout/', views.user_logout),
     path('signup/', views.user_signup, name="signup")
